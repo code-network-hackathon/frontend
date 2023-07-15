cef59050f7c541b109d29098d78dd3b36a7f3fd8
Luke Mai <mailong.c8.cva@gmail.com>
2023-07-15 19:04:39 +1000

    Adding NavBar

--- app/testingComponent/page.js
+++ app/testingComponent/page.js
@@ -1,10 +1,12 @@
 "use client";
 
 import FilterMenu from "@/components/webVersion/Filtermenu";
+import NavBar from "@/components/webVersion/NavBar";
+import ProductGrid from "@/components/webVersion/ProductGrid";
 import useFetchData from "@/hooks/useFetchData";
 
 export default function TestingPage() {
     return(
-        <FilterMenu/>
+        <NavBar/>
     )
 }
